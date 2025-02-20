0c1db65d8eb868691d028d30384f3fe7364fde0e
Thiago H Coltro <thiagohcoltro@gmail.com>
2025-02-20 18:02:53 -0300

    quarto exercício

--- app4.py
+++ app4.py
@@ -1,1 +1,15 @@
 #4o exercício
+
+x = int(input("Forneça a coordenada longitudinal: "))
+y = int(input("Forneça a coordenada latitudinal: "))
+
+if x > 0 and y > 0:
+    print("O ponto se encontra no primeiro quadrante.")
+elif x < 0 and y > 0:
+    print("O ponto se encontra no segundo quadrante.")
+elif x < 0 and y < 0:
+    print("O ponto se encontra no terceiro quadrante.")
+elif x > 0 and y < 0:
+    print("O ponto se encontra no quarto quadrante.")
+else:
+    print("O ponto está localizado no eixo ou origem.")
