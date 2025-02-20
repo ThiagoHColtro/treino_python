2bbd9555f24f813d1f09f043877239e889db33e5
Thiago H Coltro <thiagohcoltro@gmail.com>
2025-02-20 17:47:12 -0300

    terceiro exercício

---
+++ app3.py
@@ -0,0 +1,14 @@
+#3rd exercise
+
+def check(usuario, senha):
+    if usuario == "Admisso" and senha == "patinho":
+        print("Acesso permitido.")
+    else:
+        print("Os nomes de usuário e senha não correspondem.")
+
+print("Digite o nome de usuário:")
+usuario = input()
+print("Digite a senha:")
+senha = input()
+
+check(usuario, senha)
